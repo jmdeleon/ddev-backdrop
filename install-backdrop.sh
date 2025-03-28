5563a23b5a49ae6b1bbca4dee10dc5d19ac418c7
jmdeleon <jmdeleon@users.noreply.github.com>
2025-03-28 03:25:44 +0000

    Update install-backdrop.sh

--- install-backdrop.sh
+++ install-backdrop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -LJO https://github.com/backdrop/backdrop/releases/latest/download/backdrop.zip
 unzip ./backdrop.zip && rm -f backdrop.zip
-ddev config --project-type=backdrop --webroot=backdrop
+ddev config --project-type=backdrop --docroot=backdrop
 ddev start
 ddev launch
