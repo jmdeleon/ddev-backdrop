9ddb2718be1013760ac2dc6cf5639e3e6dadb194
jmdeleon <jmdeleon@users.noreply.github.com>
2025-04-06 07:44:36 +0000

    Update install-backdrop.sh

--- install-backdrop.sh
+++ install-backdrop.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-curl -LJO https://github.com/backdrop/backdrop/releases/latest/download/backdrop.zip
-unzip ./backdrop.zip && rm -f backdrop.zip
-ddev config --project-type=backdrop --docroot=backdrop
+mkdir backdrop
+cd backdrop
+ddev config --project-type=backdrop --docroot=.
+# Add the official Bee CLI add-on
+ddev add-on get backdrop-ops/ddev-backdrop-bee
 ddev start
+# Download Backdrop core
+ddev bee download-core
+# Create admin user
+ddev bee si --username=admin --password=Password123 --db-name=db --db-user=db --db-pass=db --db-host=db --auto
+# Login using `admin` user and `Password123` password
 ddev launch
